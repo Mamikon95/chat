9d4c3e99c66e8c2bbcdaa7d07063ccfa8402ea1f
Mamikon <atoyanm95@gmail.com>
2021-12-26 11:31:45 +0400

    changes

--- data/index.js
+++ data/index.js
@@ -14,6 +14,11 @@ var Phones = {
         Color: 'Purple',
         Price: '400$',
     },
+    3: {
+        Model: 'Xiaomi',
+        Color: 'Green',
+        Price: '400$',
+    },
 };
 
 exports.Phones = Phones;
