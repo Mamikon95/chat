2a30ec1e4acc3724cb9f5e508b79800c889d9250
Mamikon <atoyanm95@gmail.com>
2021-12-26 13:32:57 +0400

    change

--- phones/phone.js
+++ phones/phone.js
@@ -44,6 +44,14 @@ var Phone = function() {
             }
         }
     }
+
+    this.fill5 = function(info) {
+        for(number in info) {
+            if(this.data[number] !== 'undefined') {
+                this.data[number] = info[number];
+            }
+        }
+    }
 };
 
 module.exports = function(data) {
